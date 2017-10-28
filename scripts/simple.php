f3724f88fffe565e5cdc98d22a86b0d3c0b7f7a2
Ádám Liszkai <adaliszk@gmail.com>
2017-10-28 21:30:29 +0200

    Add 'simple' test from https://github.com/php/php-src/blob/master/Zend/bench.php

---
+++ scripts/simple.php
@@ -0,0 +1,7 @@
+<?php
+$a = 0;
+  for ($i = 0; $i < 1000000; $i++) 
+    $a++;
+  $thisisanotherlongname = 0;
+  for ($thisisalongname = 0; $thisisalongname < 1000000; $thisisalongname++) 
+    $thisisanotherlongname++;
