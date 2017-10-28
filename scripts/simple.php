7aa9b41aed0f012d00f190e091f6e23d8e8a7289
Ádám Liszkai <adaliszk@gmail.com>
2017-10-28 21:40:16 +0200

    Rename scripts to be executed in order

--- scripts/simple.php
+++
@@ -1,7 +0,0 @@
-<?php
-$a = 0;
-  for ($i = 0; $i < 1000000; $i++) 
-    $a++;
-  $thisisanotherlongname = 0;
-  for ($thisisalongname = 0; $thisisalongname < 1000000; $thisisalongname++) 
-    $thisisanotherlongname++;
